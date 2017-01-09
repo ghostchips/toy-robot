5d41db7c205ce3b84b99840499ac0e52f3ead291
Micah Boyd <micahhope@yahoo.com>
2017-01-09 12:15:07 +1100

    first commit

---
+++ app.rb
@@ -0,0 +1,17 @@
+
+# Create a talble class that determines the size of the table where the robot can roam in.
+
+class Table
+
+# Because the directions ask the coordinates for 0, 0 to be South West, the maximum boundry of the board is set with the opposite corner, North, East.
+  def initialize(north, east)
+    @north = north
+    @east = east
+  end
+
+  def table_size
+
+  end
+end
+
+table = Table.new
