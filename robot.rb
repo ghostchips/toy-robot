d1fb0b1bdb209d0e69a3a390af3f1b34c511828d
Micah Boyd <micahhope@yahoo.com>
2017-01-11 13:19:27 +1100

    all robot commands working

--- robot.rb
+++ robot.rb
@@ -1,26 +1,28 @@
 
-# robot class that is used to set and move the robot
+# robot class that is used to set and move the robot.
 
 class Robot
 
-  # when creating the robot, pass in the current table that has been created
+  # when creating the robot, pass in the current table that has been created.
   def initialize(table)
-    @table = table
+    # north defined by the northern most point in the created table
     @north = table.size.first
+    @south = 0
+    # east defined by the eastern most point in the created table
     @east = table.size.last
+    @west = 0
   end
 
   def place(x, y, facing)
-    # check to see if x, y coordinates are inside the existing table before setting them.
-    if x >= 0 || x <= @north ||
-       y >= 0 || y <= @east
+    # check to see if given x, y coordinates are inside the existing table before placing the robot.
+    if x >= @south && x <= @north &&
+       y >= @west && y <= @east
       @x = x
       @y = y
       @facing = facing
       return [@x, @y, @facing]
     else
-      # error message
-      puts "The Toy Robot must be placed within the #{table.size} table."
+      return false
     end
 
   end
@@ -46,27 +48,30 @@ class Robot
     return false
   end
 
-  def turn
+  def left
     compass = ['NORTH', 'EAST', 'SOUTH', 'WEST']
+    # moving left on 'WEST' will face you 'NORTH'
     if @facing == 'NORTH'
       @facing = 'WEST'
     else
-      # Change what direction robot is facing by moving left in the compass array based on current direction
+      # Change what direction robot is facing by moving left in the compass array based on current facing direction
     @facing = compass[compass.index(@facing) - 1]
     end
   end
 
   def right
     compass = ['NORTH', 'EAST', 'SOUTH', 'WEST']
+    # moving left on 'WEST' will face you 'NORTH'
     if @facing == 'WEST'
       @facing = 'NORTH'
     else
-      # Change what direction robot is facing by moving right in the compass array based on current direction
+      # Change what direction robot is facing by moving right in the compass array based on current facing direction
     @facing = compass[compass.index(@facing) + 1]
     end
   end
 
   def report
+    # display current location in array format
     return [@x, @y, @facing]
   end
 
